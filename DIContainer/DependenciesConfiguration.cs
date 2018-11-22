da6f35a648125d281812204712bf3e0ec4fb55cb
devvindan <devvindan@gmail.com>
2018-11-22 20:06:19 +0300

    added empty tests and basic functionality

--- DIContainer/DependenciesConfiguration.cs
+++ DIContainer/DependenciesConfiguration.cs
@@ -9,8 +9,25 @@ namespace DIContainer
     public class DependenciesConfiguration
     {
 
+        public DependenciesConfiguration()
+        {
+
+        }
+
         public void Register<TDependency, TImplementation>()
         {
+            Type dependencyType = typeof(TDependency);
+            Type implementationType = typeof(TImplementation);
+            
+            // Need to check if TImplementation type is abstract.
+
+            // Need to check for recursive creation.
+
+            // Need to check for generics.
+
+            // Need to check for IEnumerable and multiple TImplementations.
+
+
             throw new NotImplementedException();
         }
 
--- DIUnitTests/UnitTests.cs
+++ DIUnitTests/UnitTests.cs
@@ -1,13 +1,53 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DIContainer;
 
 namespace DIUnitTests
 {
+    [TestClass]
     public class UnitTests
     {
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestConfigurationValidation()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestRecursiveDependencies()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestSingletonDependencies()
+        {
+            // must be multithreading-protected
+
+        }
+
+        [TestMethod]
+        public void TestMultipleImplementations()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestGenericDependencies()
+        {
+
+        }
 
     }
 }
